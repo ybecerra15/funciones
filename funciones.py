5292d50b9c438b086290c24c518225c788e5f2e2
yeisson Becerra Ochoa <ybecerra15@estudiantes.areandina.edu.co>
2022-06-29 20:40:04 -0500

    mi segunda tercera funcion agosto

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+from errno import ESTALE
 import os
 os.system("cls")
 
@@ -5,5 +6,13 @@ def nombre (): # mi primera funcion.
     a=3
     b=4
     c=a+b
-    print(c)
+    print("el resultado de la suma es:",c)
 nombre()
+
+def resta():
+    a=2
+    b=1
+    c=a-b
+    print("el resulatdo de la resta es :",c)
+resta()
+    
